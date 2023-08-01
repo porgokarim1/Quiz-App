59a22e0ae820fca3df19d76d158f3de7e2d22d6c
porgokarim1 <60364286+porgokarim1@users.noreply.github.com>
2023-08-01 00:37:31 -0400

    Taking phone user screen resolution in consideration

---
+++ app.js
@@ -0,0 +1,72 @@
+const quizQuestions = [{
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+}, {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "b",
+}, {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Language",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+}, {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+}, ];
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+let currentQuiz = 0;
+let score = 0;
+const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
+const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+        if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+};
+const loadQuiz = () => {
+    deselectAnswers();
+    const currentquizQuestions = quizQuestions[currentQuiz];
+    questionElement.innerText = currentquizQuestions.question;
+    a_text.innerText = currentquizQuestions.a;
+    b_text.innerText = currentquizQuestions.b;
+    c_text.innerText = currentquizQuestions.c;
+    d_text.innerText = currentquizQuestions.d;
+};
+loadQuiz();
+submitButton.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizQuestions[currentQuiz].correct) score++;
+        currentQuiz++;
+        if (currentQuiz < quizQuestions.length) loadQuiz();
+        else {
+            quiz.innerHTML = ` <h2>You answered ${score}/${quizQuestions.length} questions correctly</h2> 
+                <button onclick="history.go(0)">Play Again</button> `
+                // location.reload() won't work in CodePen for security reasons; } } });
+        }
+    }
+});
